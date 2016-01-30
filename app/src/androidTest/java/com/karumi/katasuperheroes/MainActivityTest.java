b53316ac2a8cf44ab6b8cac21d20003e9a947736
Manolo Vera <manolo.vn@gmail.com>
2016-01-30 12:08:08 +0100

    another test added

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -17,14 +17,18 @@
 package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
+import com.karumi.katasuperheroes.matchers.RecyclerViewItemsCountMatcher;
 import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
+import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
 
 import org.junit.Rule;
@@ -32,16 +36,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.karumi.katasuperheroes.matchers.RecyclerViewItemsCountMatcher.recyclerViewHasItemCount;
 import static org.hamcrest.CoreMatchers.not;
 import static org.mockito.Mockito.when;
 
@@ -49,6 +58,7 @@ import static org.mockito.Mockito.when;
 @LargeTest
 public class MainActivityTest {
 
+    public static final int SUPERHEROES_COUNT = 200;
     @Rule
     public DaggerMockRule<MainComponent> daggerRule =
             new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
@@ -81,7 +91,7 @@ public class MainActivityTest {
 
     @Test
     public void shouldNotShowEmptyCaseIfThereAreSuperHeros() {
-        givenThereAreOneSuperHero();
+        givenThereIsOneSuperHero();
 
         startActivity();
 
@@ -90,18 +100,59 @@ public class MainActivityTest {
 
     @Test
     public void shouldShowOneSuperHero() {
-        givenThereAreOneSuperHero();
+        givenThereIsOneSuperHero();
 
         startActivity();
 
         onView(withId(R.id.tv_super_hero_name)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void shouldShowManySuperHeroes() {
+        givenThereAreSomeSuperHeroes(SUPERHEROES_COUNT);
+
+        startActivity();
+
+        onView(withId(R.id.recycler_view))
+                .check(matches(recyclerViewHasItemCount(SUPERHEROES_COUNT)));
+    }
+
+    @Test
+    public void shouldShowSuperHeroNameOnList() {
+        List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes(SUPERHEROES_COUNT);
+
+        startActivity();
+
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+                .withItems(superHeroes)
+                .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+                    @Override
+                    public void check(SuperHero item, View view, NoMatchingViewException e) {
+                        matches(hasDescendant(withText(item.getName()))).check(view, e);
+                    }
+                });
+    }
+
+    private List<SuperHero> givenThereAreSomeSuperHeroes(int count) {
+        List<SuperHero> superHeroes = new ArrayList<>();
+        for (int i = 0 ; i < count ; i++) {
+            SuperHero superHero = new SuperHero(
+                    "Super perrete " + i,
+                    "anyPhoto",
+                    i % 2 == 0,
+                    "Description"
+            );
+            superHeroes.add(superHero);
+        }
+        when(repository.getAll()).thenReturn(superHeroes);
+        return superHeroes;
+    }
+
     private void givenThereAreNoSuperHeroes() {
         when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
     }
 
-    private void givenThereAreOneSuperHero() {
+    private void givenThereIsOneSuperHero() {
         when(repository.getAll()).thenReturn(Arrays.asList(
                 new SuperHero("Super perrete",
                         "http://elhistorias.com/wp-content/uploads/2010/04/super_perrete.jpg",
